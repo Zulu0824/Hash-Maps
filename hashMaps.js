6fb3e897ad697a9d5c42d80a9b7d54d43cd92e94
Zulu0824 <sriharsha.patta@gmail.com>
2026-09-21 22:33:01 +0530

    Add entries() array

--- hashMaps.js
+++ hashMaps.js
@@ -120,4 +120,17 @@ class HashMap {
     }
     return valueArray;
   }
+  entries() {
+    const entriesArray = [];
+
+    for (let i = 0; i < this.buckets.length; i++) {
+      const bucket = this.buckets[i];
+      if (bucket) {
+        for (let j = 0; j < bucket.length; j++) {
+          entriesArray.push([bucket[j][0], bucket[j][1]]);
+        }
+      }
+    }
+    return entriesArray;
+  }
 }
