5a09d5516d1175cefd565041896ddec224ea9d93
Zulu0824 <sriharsha.patta@gmail.com>
2026-09-21 23:15:19 +0530

    Add tests to main.js

--- main.js
+++ main.js
@@ -16,3 +16,21 @@ test.set("kite", "pink");
 test.set("lion", "golden");
 
 console.log(test.entries());
+
+console.log("The value is " + test.get("apple"));
+console.log("The value is " + test.get("kite"));
+
+console.log(test.has("dog"));
+console.log(test.has("frog"));
+console.log(test.has("orange"));
+
+console.log(test.remove("grape") + " key removed");
+console.log(test.remove("tiger"));
+
+console.log(test.length());
+
+// console.log(test.clear());
+
+console.log(test.keys());
+
+console.log(test.values());
